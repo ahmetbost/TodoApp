cf915783f52467950bce29bbdb8dde54a95ebee0
Bost <ahmetbostanciogluu@gmail.com>
2025-04-30 12:39:21 +0300

    Initial Commit

---
+++ To-Do/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do
+//
+//  Created by Ahmet Bostancıoğlu on 30.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To-Do
+//
+//  Created by Ahmet Bostancıoğlu on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
